a31767834cb4160c6712a70e3106be75a0e33303
Braxton Cuneo <braxton.s.cuneo@gmail.com>
2025-04-07 15:05:53 -0700

    Input event demo

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,27 +4,50 @@ using System;
 
 public partial class Player : RigidBody3D
 {
+	bool MoveForward;
+	bool MoveBackward;
+	bool MoveLeft;
+	bool MoveRight;
+
 	public override void _Ready()
 	{
+		MoveForward = false;
+		MoveBackward = false;
+		MoveRight = false;
+		MoveLeft = false;
 	}
-	
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsAction("move_right"))
+		{
+			MoveRight = @event.IsPressed();
+		} else if (@event.IsAction("move_left")) {
+			MoveLeft = @event.IsPressed();
+		} else if (@event.IsAction("move_forward")) {
+			MoveForward= @event.IsPressed();
+		} else if (@event.IsAction("move_backward")) {
+			MoveBackward = @event.IsPressed();
+		}
+	}
+
 	public override void _Process(double delta)
 	{
 		Vector3 movement_force = new Vector3(0,0,0);
-		if (Input.IsActionPressed("move_right")) {
+		if (MoveRight) {
 			movement_force.X += 1;
 		}
-		if (Input.IsActionPressed("move_left")) {
+		if (MoveLeft) {
 			movement_force.X -= 1;
 		}
-		if (Input.IsActionPressed("move_forward")) {
+		if (MoveForward) {
 			movement_force.Z -= 1;
 		}
-		if (Input.IsActionPressed("move_backward")) {
+		if (MoveBackward) {
 			movement_force.Z += 1;
 		}
 		float scale = (float)delta*10;
 		ApplyCentralImpulse(movement_force*new Vector3(scale,scale,scale));
 	}
-	
+
 }
