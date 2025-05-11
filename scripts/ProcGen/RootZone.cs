877a8d3230f94f3bb7003bded21a0da19f2fe43d
Braxton Cuneo <braxton.s.cuneo@gmail.com>
2025-05-11 00:07:40 -0700

    Procgen WIP

---
+++ scripts/ProcGen/RootZone.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+using System.Collections.Generic;
+
+namespace ProcGen {
+
+public partial class RootZone : Zone
+{
+
+	RootZone(ZoneCoord coord) : base(coord) {}
+	
+	
+}
+
+}
---
+++ scripts/ProcGen/SolidZone.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+namespace ProcGen {
+
+public partial class SolidZone : Zone
+{
+	
+	public SolidZone(ZoneCoord coord) : base(coord) {
+		
+	}
+	
+	
+	
+}
+
+}
