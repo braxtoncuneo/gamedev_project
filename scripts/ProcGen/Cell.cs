2b60e69f7826df0edba46ece1e8efd4e72b36d0c
Braxton Cuneo <braxton.s.cuneo@gmail.com>
2025-05-27 19:16:03 -0700

    Workable trees

--- scripts/ProcGen/Cell.cs
+++ scripts/ProcGen/Cell.cs
@@ -5,36 +5,60 @@ using System.Collections.Generic;
 
 namespace ProcGen {
 
-public interface ICell <Coord,DataType>
+public interface ICell<Coord,DataType>
 {
 	public Coord   Location {get;}
 	public DataType Data {get;set;}
+	public IAdjCell<Coord,DataType> AsAdj() {
+		return this as IAdjCell<Coord,DataType>;
+	}
+	public IDirAdjCell<Coord,DataType> AsDirAdj() {
+		return this as IDirAdjCell<Coord,DataType>;
+	}
+	public ITreeCell<Coord,DataType> AsTree() {
+		return this as ITreeCell<Coord,DataType>;
+	}
 }
 
-public interface IAdjCell <CellKind,Coord,DataType> : ICell<Coord,DataType>
-	where CellKind : IAdjCell <CellKind,Coord,DataType>, ICell<Coord,DataType>
+public interface IAdjCell<Coord,DataType> : ICell<Coord,DataType>
 {
-	public IEnumerable<CellKind> Adj();
+	public IEnumerable<IAdjCell<Coord,DataType>> Adj();
 }
 
-public interface IDagCell <CellKind,Coord,DataType> : ICell<Coord,DataType>
-	where CellKind : IDagCell <CellKind,Coord,DataType>, ICell<Coord,DataType>
+public interface IDirAdjCell<Coord,DataType> : ICell<Coord,DataType>
 {
-	public IEnumerable<CellKind> InAdj();
-	public IEnumerable<CellKind> OutAdj();
+	public IEnumerable<IDirAdjCell<Coord,DataType>> InAdj();
+	public IEnumerable<IDirAdjCell<Coord,DataType>> OutAdj();
 }
 
-public interface ITreeCell <CellKind,Coord,DataType> : ICell<Coord,DataType>
-	where CellKind : ITreeCell <CellKind,Coord,DataType>, ICell<Coord,DataType>
+public interface ITreeCell<Coord,DataType> : ICell<Coord,DataType>
 {
-	public CellKind Parent();
-	public IEnumerable<CellKind> Children();
+	public ITreeCell<Coord,DataType> Parent();
+	public IEnumerable<ITreeCell<Coord,DataType>> Children();
+	public ITreeCell<Coord,DataType> Root() {
+		ITreeCell<Coord,DataType> iter = this;
+		ITreeCell<Coord,DataType> parent = iter.Parent();
+		while (parent is not null) {
+			iter = parent;
+			parent = iter.Parent();
+		}
+		return iter;
+	}
+	public IEnumerable<ITreeCell<Coord,DataType>> Descendants() {
+		foreach (var child in Children()) {
+			foreach (var desc in child.Descendants()) {
+				yield return desc;
+			}
+		}
+	}
+	public IEnumerable<ITreeCell<Coord,DataType>> Leaves() {
+		return Root().Descendants();
+	}
 }
 
-public interface ICellDistance <CellKind,Coord,DataType,Length> : ICell<Coord,DataType>
-	where CellKind : ICellDistance <CellKind,Coord,DataType,Length>, ICell<Coord,DataType>
+public interface ICellDistance<Coord,DataType,Length> : ICell<Coord,DataType>
 {
-	public Length DistanceTo(CellKind otherCell);
+	public Length DistanceTo(ICellDistance<Coord,DataType,Length> otherCell);
 }
 
 }
